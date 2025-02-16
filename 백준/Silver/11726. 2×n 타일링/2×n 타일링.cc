80c35d5b783233387841c09a969c41bd26ba254d
김형욱 <khuk0114@naver.com>
2025-02-17 07:34:11 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+const int m = 10007;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int dp[1001] = { 0 };
+
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + dp[i - 2]) % m;
+	}
+
+	cout << dp[n];
+
+	return 0;
+
+}
